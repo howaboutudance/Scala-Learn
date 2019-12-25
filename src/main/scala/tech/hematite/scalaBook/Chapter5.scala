42c470d67bbd8e015efcebcf9f693adb37bf3b25
Michael Penhallegon <mike@hematite.tech>
2019-12-25 14:48:20 -0800

    addd operator equivlancy code

--- src/main/scala/tech/hematite/scalaBook/Chapter5.scala
+++ src/main/scala/tech/hematite/scalaBook/Chapter5.scala
@@ -6,6 +6,11 @@ object Chapter5 {
     def runChapter5(): Unit = {
         // String & Character Literals
         characterLiterals()
+        stringLiterals()
+        stringInterpolation()
+
+        // operator eqilvancy  that foo.bar(0) is equiv to foo bar 0
+        operatorEquilvancy()
     }
 
     private def characterLiterals(): Unit = {
@@ -20,9 +25,6 @@ object Chapter5 {
     private def stringLiterals(): Unit = {
         // string literals like emoji
         println("👨🏿‍💻")
-        println("Chapter 5 complete!")
-
-
     }
 
     private def stringInterpolation(): Unit = {
@@ -31,9 +33,22 @@ object Chapter5 {
         println(s"${strExample}")
 
         // using string format interpoliation
-        println(f"$strExample\t$strDecimal")
+        println(f"$strExample\t$strDecimal.8")
 
         //using an identfier
         println(f"$symIdent")
+    }
+
+    private def operatorEquilvancy(): Unit = {
+        val valArray: Array[Int] = Array(1, 2, 3)
+        if(valArray.indexOf(3) >= 0){
+            println("index found")
+        } else {
+            println("index not found")
+        }
+
+        if((valArray indexOf 3) >= 0 ){
+            println(s"index found in op notation: ${valArray indexOf 3}")
+        }
     }
 }
